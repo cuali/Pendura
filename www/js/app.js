a8d7b5c96ebed32fa686ddb1e849a57b21546cdd
Alain Bearez <A@cua.li>
2015-02-20 12:08:55 -0300

    ionic already does what angular touch would do: avoid conflicts

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,18 @@
-// Ionic pendura App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
 // 'pendura' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'pendura.services' is found in services.js
-// 'pendura.controllers' is found in controllers.js
-angular.module('pendura', ['ionic', 'ngCordova', 'ngTouch', 'uuid4', 'pendura.controllers', 'pendura.services'])
+angular.module('pendura', ['ionic', 'ngCordova', 'uuid4', 'pendura.controllers', 'pendura.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.styleDefault()
     }
-  });
+  })
 })
 
 .config(function($compileProvider, $stateProvider, $urlRouterProvider) {
@@ -26,8 +20,6 @@ angular.module('pendura', ['ionic', 'ngCordova', 'ngTouch', 'uuid4', 'pendura.co
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
   // setup an abstract state for the tabs directive
@@ -77,9 +69,9 @@ angular.module('pendura', ['ionic', 'ngCordova', 'ngTouch', 'uuid4', 'pendura.co
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/dash')
 
-});
+})
